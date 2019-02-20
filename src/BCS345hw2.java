f30a11bac5eb0a6c803cc18c34de38b7131bbcae
Vinny <Vinny@DESKTOP-BKKS7PL>
2019-02-20 16:02:51 -0500

    Task 2 Logic complete. Highest Numbers in: column/row Totals for: Columns/Rows/all entries Print data

--- src/BCS345hw2.java
+++ src/BCS345hw2.java
@@ -1,6 +1,7 @@
-import java.util.*; 
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.regex.Pattern;
 
 
 public class BCS345hw2 {
@@ -14,36 +15,132 @@ public class BCS345hw2 {
         return 0; 
     } 
     
-    public static void main(String[] args) 
+    public static void main(String[] args) throws FileNotFoundException 
     {
-        int SIDES = 6;
-        Scanner s = new Scanner(System.in);
+
+        //Task 1
+//        
+//        int SIDES = 6;
+//        Scanner s = new Scanner(System.in);
+//        
+//        System.out.print("How many times would you like to roll: ");
+//        int numRolls = s.nextInt();
+//        int [] allRolls = new int[numRolls];
+//        int [] numOccurrences = new int[SIDES];
+//        double [] frequencyOfNums = new double[SIDES];
+//        
+//        for(int i=0;i<numRolls;i++)
+//        {
+//            int roll = (int)(Math.random() * SIDES)+1;
+//            allRolls[i] = roll;
+//            if (hm.containsKey(allRolls[i]) ) 
+//                hm.put(allRolls[i], hm.get(allRolls[i]) + 1); 
+//            else hm.put(allRolls[i] , 1); 
+//        }
+//        
+//        System.out.printf("%-13s%-24s%s\n","Face Value","Number Of Occurrences","Frequency");
+//        for(int i=0; i < SIDES;i++)
+//        {
+//            numOccurrences[i] = getNumOccurrences(i+1);
+//            frequencyOfNums[i] = ((double)numOccurrences[i])/(int)numRolls;
+//            String freq = String.format("%.6f", frequencyOfNums[i]);
+//            System.out.printf("%5d%18d%22s\n",(i+1),numOccurrences[i],freq);
+//        
+//        }
+//        
+        //Task 2
         
-        System.out.print("How many times would you like to roll: ");
-        int numRolls = s.nextInt();
-        int [] allRolls = new int[numRolls];
-        int [] numOccurrences = new int[SIDES];
-        double [] frequencyOfNums = new double[SIDES];
-        
-        for(int i=0;i<numRolls;i++)
+        double[][] multiArray = new double[5][4];
+        File file = 
+        new File("D:\\OneDrive - Farmingdale\\BCS345\\BCS345hw2\\hw2data.txt"); 
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter(Pattern.compile("(\\n)| "));
+        int row = 0;
+        int column = 0;
+        while (sc.hasNext())
         {
-            int roll = (int)(Math.random() * SIDES)+1;
-            allRolls[i] = roll;
-            if (hm.containsKey(allRolls[i]) ) 
-                hm.put(allRolls[i], hm.get(allRolls[i]) + 1); 
-            else hm.put(allRolls[i] , 1); 
+            multiArray[row][column] = Double.valueOf(sc.next());
+            
+            column++;
+            if(column == 4)
+            {
+                row++;
+                column = 0;
+            }
         }
-        
-        System.out.printf("%-13s%-24s%s\n","Face Value","Number Of Occurrences","Frequency");
-        for(int i=0; i < SIDES;i++)
+        printArray(multiArray);
+        double total = getTotal(multiArray);
+        double rowTotal = getRowTotal(multiArray, 2);
+        double columnTotal = getColumnTotal(multiArray,2);
+        double highestColNum = getHighestInColumn(multiArray, 2);
+        double highestRowNum = getHighestInRow(multiArray,3);
+        System.out.println(total);
+        System.out.println(rowTotal);
+        System.out.println(columnTotal);
+        System.out.println(highestColNum);
+        System.out.println(highestRowNum);
+    }
+    public static double getHighestInColumn(double[][] x, int column)
+    {
+        double highestNum = x[0][column];
+        for (double[] y : x) {
+            if (highestNum < y[column]) {
+                highestNum = y[column];
+            }
+        }
+        return highestNum;
+    }
+    public static double getHighestInRow(double[][] x, int row)
+    {
+        double highestNum = x[row][0];
+        for(int i = 0; i < x[row].length;i++)
         {
-            numOccurrences[i] = getNumOccurrences(i+1);
-            frequencyOfNums[i] = ((double)numOccurrences[i])/(int)numRolls;
-            String freq = String.format("%.6f", frequencyOfNums[i]);
-            System.out.printf("%5d%18d%22s\n",(i+1),numOccurrences[i],freq);
-        
+            if(highestNum < x[row][i])
+            {
+                highestNum = x[row][i];
+            }
         }
+            
+        return highestNum;
         
+    }
+    public static double getColumnTotal(double[][] x, int column)
+    {
+        double total = 0;
+        for (double[] y : x) {
+            total += y[column];
+        }
+        return total;
+    }    
+    public static double getRowTotal(double[][] x, int row)
+    {
+        double total = 0;
+        for(int i = 0; i < x[row].length;i++)
+        {
+            total += x[row][i];
+        }
+            
+        return total;
+        
+    }
+    public static double getTotal(double[][] x)
+    {
+        double total = 0;
+        for (double[] y : x) {
+            for (int j = 0; j < y.length; j++) {
+                total += y[j];
+            }
+        }
+        return total;
+    }
+    public static void printArray(double[][] x)
+    {
+        for (double[] y : x) {
+            for (int j = 0; j < y.length; j++) {
+                System.out.printf("%.02f ", y[j]);
+            }
+            System.out.println();
+        }
     }
     
 }
